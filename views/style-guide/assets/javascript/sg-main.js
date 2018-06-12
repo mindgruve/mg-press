f601179097835c48cc30fe4b583863e4afa3a01a
wmarchment <mon@westleymon.com>
2018-06-12 11:20:10 -0700

    pinned sub navigation with subnav highlighting

--- views/style-guide/assets/javascript/sg-main.js
+++ views/style-guide/assets/javascript/sg-main.js
@@ -2,7 +2,7 @@
  * Tabs
  */
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
     // store tabs variable
     var tabGroups = document.querySelectorAll(".sg-tabs");
@@ -10,8 +10,8 @@ window.addEventListener("load", function() {
     function setupTabs(group) {
         var tabEls = group.querySelectorAll('ul.sg-tabs-list li');
 
-        for(var i = 0; i < tabEls.length; i++) {
-            tabEls[i].addEventListener("click", function(e) {
+        for (var i = 0; i < tabEls.length; i++) {
+            tabEls[i].addEventListener("click", function (e) {
                 for (var i = 0; i < tabEls.length; i++) {
                     tabEls[i].classList.remove("sg-active");
                 }
@@ -40,5 +40,67 @@ window.addEventListener("load", function() {
     for (var i = 0; i < tabGroups.length; i++) {
         setupTabs(tabGroups[i]);
     }
+
+});
+
+/**
+ * Pinned Sub Nav
+ */
+window.addEventListener("load", function () {
+    var pageNavWrapper = document.querySelectorAll('.sg-page-nav-wrapper')[0];
+    var pageNav = pageNavWrapper.querySelectorAll('.sg-page-nav')[0];
+    var pageNavOffset = pageNavWrapper.offsetTop;
+
+    function pinPageNav() {
+        var scrollTop = window.pageYOffset;
+        if (scrollTop >= pageNavOffset) {
+            pageNav.classList.add('pinned')
+        } else {
+            pageNav.classList.remove('pinned')
+        }
+
+        window.requestAnimationFrame(pinPageNav);
+    }
+
+    window.requestAnimationFrame(pinPageNav);
+});
+
+/**
+ * Sub Nav Active States
+ */
+window.addEventListener("load", function () {
+    var subNavItems = Array.prototype.slice.call(document.querySelectorAll('.sg-page-nav a'));
+    // reverse array for cascading conditional from bottom up.
+    subNavItems.reverse();
+
+    var subNavTriggerPoints = [];
+
+    for (var i = 0; i < subNavItems.length; i++) {
+        var href = subNavItems[i].hash.substring(1);
+
+        subNavTriggerPoints.push(document.getElementById(href).offsetTop)
+    }
+
+    function highlightActive() {
+        var scrollTop = window.pageYOffset;
+
+        for (var i = 0; i < subNavTriggerPoints.length; i++) {
+            if (scrollTop > subNavTriggerPoints[i] - 90) {
+                subNavItems[i].parentNode.classList.add('sg-active');
+
+                for (var d = 0; d < subNavItems.length; d++) {
+                    if (d !== i) {
+                        subNavItems[d].parentNode.classList.remove('sg-active');
+                    }
+                }
+                break;
+            }
+        }
+
+        window.requestAnimationFrame(highlightActive)
+    }
+
+    window.requestAnimationFrame(highlightActive);
+
 
 });
